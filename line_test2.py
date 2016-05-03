a336e4b3c2aa2a96386a841b33b32efbaaae8fac
Romain Mari <romain.mari@gmail.com>
2016-05-03 18:11:48 +0100

    lines and circles

--- line_test2.py
+++ line_test2.py
@@ -5,26 +5,10 @@ from PyQt4.QtOpenGL import QGLWidget
 import OpenGL.GL as gl
 import OpenGL.arrays.vbo as glvbo
 
-VS = """
-attribute vec3 a_position;
-attribute vec4 a_fg_color;
+import shaders
 
-varying vec4 v_fg_color;
 
-void main (void) {
-    gl_Position = vec4(a_position,1.0);
-    v_fg_color  = a_fg_color;
-}
-"""
 
-FS = """
-varying vec4 v_fg_color;
-
-void main()
-{
-    gl_FragColor = v_fg_color;
-}
-"""
 
 # from http://cyrille.rossant.net/shaders-opengl/
 def compile_vertex_shader(source):
@@ -38,6 +22,7 @@ def compile_vertex_shader(source):
         raise RuntimeError(gl.glGetShaderInfoLog(vertex_shader))
     return vertex_shader
 
+
 # from http://cyrille.rossant.net/shaders-opengl/
 def compile_fragment_shader(source):
     """Compile a fragment shader from source."""
@@ -49,6 +34,7 @@ def compile_fragment_shader(source):
     if not(result):
         raise RuntimeError(gl.glGetShaderInfoLog(fragment_shader))
     return fragment_shader
+
 
 # from http://cyrille.rossant.net/shaders-opengl/
 def link_shader_program(vertex_shader, fragment_shader):
@@ -64,6 +50,15 @@ def link_shader_program(vertex_shader, fragment_shader):
     return program
 
 
+def make_shader_program(vertex_source, fragment_source):
+    # compile the vertex shader
+    vs = compile_vertex_shader(vertex_source)
+    # compile the fragment shader
+    fs = compile_fragment_shader(fragment_source)
+    # compile the vertex shader
+    return link_shader_program(vs, fs)
+
+
 class GLPlotWidget(QGLWidget):
     # default window size
     width, height = 600, 600
@@ -76,10 +71,10 @@ class GLPlotWidget(QGLWidget):
 
     def keyPressEvent(self, event):
         e = event.key()
-        # if e == QtCore.Qt.Key_Return:  # repeat previous action
-        self.set_data()
-        # self.vbo = glvbo.VBO(self.data, usage='GL_STREAM_DRAW_ARB')
+        if e == QtCore.Qt.Key_Return:  # repeat previous action
+            self.set_data()
         self.vbo.set_array(self.data)
+        self.vbo_c.set_array(self.data_c)
 
         self.updateGL()
 
@@ -95,39 +90,62 @@ class GLPlotWidget(QGLWidget):
         normals /= np.linalg.norm(normals, axis=1)[:, np.newaxis]
         a_position = np.empty(shape=(6*n, 3))
         a_position[::6] = line_ends[:, :3]+thicknesses*normals
-        a_position[1::6] = line_ends[:,:3]-thicknesses*normals
-        a_position[2::6] = line_ends[:,3:]+thicknesses*normals
+        a_position[1::6] = line_ends[:, :3]-thicknesses*normals
+        a_position[2::6] = line_ends[:, 3:]+thicknesses*normals
         a_position[3::6] = a_position[1::6]
         a_position[4::6] = a_position[2::6]
         a_position[5::6] = line_ends[:, 3:]-thicknesses*normals
-        colors = np.repeat(np.random.uniform(0.85, 1.00, (n, 4)),6, axis=0)
+        colors = np.repeat(np.random.uniform(0.2, 1.00, (n, 4)), 6, axis=0)
         self.data = np.hstack((a_position, colors)).astype(np.float32)
-        self.count = self.data.shape[0]
+
+        self.data_c = np.zeros((n, 12), dtype=np.float32)
+        self.data_c[:, :3] = 0.45 * np.random.randn(n, 3)
+        self.data_c[:, 3:7] = np.random.uniform(0.85, 1.00, (n, 4))
+        self.data_c[:, 7:11] = 1, 0, 0, 1
+        self.data_c[:, 11] = 10  #np.random.uniform(400, 100, n)
 
     def initializeGL(self):
         """Initialize OpenGL, VBOs, upload data on the GPU, etc.
         """
         # background color
-        gl.glClearColor(0, 0, 0, 0)
+        gl.glClearColor(1, 1, 1, 1)
+        gl.glEnable(gl.GL_VERTEX_PROGRAM_POINT_SIZE)
+        gl.glEnable(gl.GL_DEPTH_TEST)
+
         # create a Vertex Buffer Object with the specified data
         self.vbo = glvbo.VBO(self.data, usage='GL_STREAM_DRAW_ARB')
-        # compile the vertex shader
-        vs = compile_vertex_shader(VS)
-        # compile the fragment shader
-        fs = compile_fragment_shader(FS)
-        # compile the vertex shader
-        self.shaders_program = link_shader_program(vs, fs)
+        self.shaders_program = make_shader_program(shaders.s_vert,
+                                                   shaders.s_frag)
         self.position_location = gl.glGetAttribLocation(self.shaders_program,
                                                         'a_position')
         self.color_location = gl.glGetAttribLocation(self.shaders_program,
                                                      'a_fg_color')
 
+        self.vbo_c = glvbo.VBO(self.data_c, usage='GL_STREAM_DRAW_ARB')
+        self.shaders_program_c = make_shader_program(shaders.c_vert,
+                                                     shaders.c_frag)
+        self.c_position_location = gl.glGetAttribLocation(self.shaders_program_c,
+                                                          'a_position')
+        self.c_bg_color_location = gl.glGetAttribLocation(self.shaders_program_c,
+                                                          'a_bg_color')
+        self.c_fg_color_location = gl.glGetAttribLocation(self.shaders_program_c,
+                                                          'a_fg_color')
+        self.c_size_location = gl.glGetAttribLocation(self.shaders_program_c,
+                                                      'a_size')
+        self.u_size_loc = gl.glGetUniformLocation(self.shaders_program_c,
+                                             'u_size')
+        self.u_linewidth_loc = gl.glGetUniformLocation(self.shaders_program_c,
+                                                  'u_linewidth')
+        self.u_antialias_loc = gl.glGetUniformLocation(self.shaders_program_c,
+                                                  'u_antialias')
+
+
     def paintGL(self):
         """Paint the scene.
         """
         # clear the buffer
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-        # bind the VBO
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        # # bind the VBO
         self.vbo.bind()
         # tell OpenGL that the VBO contains an array of vertices
         gl.glEnableVertexAttribArray(self.position_location)
@@ -135,18 +153,48 @@ class GLPlotWidget(QGLWidget):
 
         stride = 7*4
         # prepare the shader
-        # these vertices contain 2 single precision coordinates
+        # these vertices contain 3 single precision coordinates
         gl.glVertexAttribPointer(self.position_location,
                                  3, gl.GL_FLOAT,
                                  gl.GL_FALSE, stride, self.vbo)
         gl.glVertexAttribPointer(self.color_location,
-                                 3, gl.GL_FLOAT,
+                                 4, gl.GL_FLOAT,
                                  gl.GL_FALSE, stride, self.vbo+12)
         gl.glUseProgram(self.shaders_program)
         # draw "count" points from the VBO
         gl.glDrawArrays(gl.GL_TRIANGLES, 0, len(self.data))
         self.vbo.unbind()
 
+        self.vbo_c.bind()
+        # tell OpenGL that the VBO contains an array of vertices
+        gl.glEnableVertexAttribArray(self.c_position_location)
+        gl.glEnableVertexAttribArray(self.c_bg_color_location)
+        gl.glEnableVertexAttribArray(self.c_fg_color_location)
+        gl.glEnableVertexAttribArray(self.c_size_location)
+
+        stride = 12*4
+        # prepare the shader
+        # these vertices contain 2 single precision coordinates
+        gl.glVertexAttribPointer(self.c_position_location,
+                                 3, gl.GL_FLOAT,
+                                 gl.GL_FALSE, stride, self.vbo_c)
+        gl.glVertexAttribPointer(self.c_bg_color_location,
+                                4, gl.GL_FLOAT,
+                                gl.GL_FALSE, stride, self.vbo_c+12)
+        gl.glVertexAttribPointer(self.c_fg_color_location,
+                                 4, gl.GL_FLOAT,
+                                 gl.GL_FALSE, stride, self.vbo_c+28)
+        gl.glVertexAttribPointer(self.c_size_location,
+                                 1, gl.GL_FLOAT,
+                                 gl.GL_FALSE, stride, self.vbo_c+44)
+        # print(self.vbo_c.data[0])
+        gl.glUseProgram(self.shaders_program_c)
+        gl.glUniform1f(self.u_size_loc, 1)
+        gl.glUniform1f(self.u_antialias_loc, 1)
+        gl.glUniform1f(self.u_linewidth_loc, 1)
+        # draw "count" points from the VBO
+        gl.glDrawArrays(gl.GL_POINTS, 0, len(self.data_c))
+        self.vbo_c.unbind()
 
     def resizeGL(self, width, height):
         """Called upon window resizing: reinitialize the viewport.
@@ -177,6 +225,7 @@ if __name__ == '__main__':
             self.setGeometry(100, 100, self.widget.width, self.widget.height)
             self.setCentralWidget(self.widget)
             self.show()
+
         def keyPressEvent(self, event):
             self.widget.keyPressEvent(event)
             self.update()
