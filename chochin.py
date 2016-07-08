74189ebe832c7584901bb67c5f57aa0567c02ac8
Romain Mari <romain.mari@gmail.com>
2016-07-08 10:20:24 +0100

    removing macron in window title.

--- chochin.py
+++ chochin.py
@@ -105,7 +105,7 @@ if __name__ == '__main__':
                              100,
                              self.datawidget.width,
                              self.datawidget.height)
-            self.setWindowTitle("Chōchin - " + filename)
+            self.setWindowTitle("Chochin - " + filename)
             self.setCentralWidget(self.datawidget)
 
             self.scene_info_widget = SceneInfoWidget(self, 180, 120)
